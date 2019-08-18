d73213631672a0ad2b1c11886da513d7496cb14a
Vlad Bokov <razum2um@mail.ru>
2019-08-19 02:54:22 +0700

    Add zstd patch support and remove bottle refs

--- Formula/squashfs.rb
+++ Formula/squashfs.rb
@@ -5,13 +5,7 @@ class Squashfs < Formula
   sha256 "0d605512437b1eb800b4736791559295ee5f60177e102e4d4ccd0ee241a5f3f6"
   revision 2
 
-  bottle do
-    cellar :any
-    sha256 "c48d6bf745747564c7b8736c1f703165d0787415d54c09680e272e2d68fdd572" => :mojave
-    sha256 "59fe238379463c52b0e219192139180be496fcc20adc734a84699f34191461c3" => :high_sierra
-    sha256 "2e015389c160a1094f0995e3d5b11f54b0549cbab7a506cbb2706608213735a6" => :sierra
-  end
-
+  depends_on "zstd"
   depends_on "lz4"
   depends_on "lzo"
   depends_on "xz"
@@ -20,13 +14,20 @@ class Squashfs < Formula
   # Also clang fixes, extra endianness knowledge and a bundle of other macOS fixes.
   # Originally from https://github.com/plougher/squashfs-tools/pull/3
   patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/05ae0eb1/squashfs/squashfs-osx-bundle.diff"
+    url "https://raw.githubusercontent.com/lunatic-cat/homebrew-tap/680daa95f7d52736314dee3cfceae7e094f7befa/patches/squashfs-osx-bundle.diff"
     sha256 "276763d01ec675793ddb0ae293fbe82cbf96235ade0258d767b6a225a84bc75f"
+  end
+
+  # Taken from here https://github.com/valyala/gozstd/blob/master/zstd/contrib/linux-kernel/0006-squashfs-tools-Add-zstd-support.patch
+  patch do
+    url "https://raw.githubusercontent.com/lunatic-cat/homebrew-tap/680daa95f7d52736314dee3cfceae7e094f7befa/patches/0006-squashfs-tools-Add-zstd-support.patch"
+    sha256 "2fe7958af8e51aa27dac89c4a24e76a738634c11166362c65c43fe7e2cacecce"
   end
 
   def install
     args = %W[
       EXTRA_CFLAGS=-std=gnu89
+      ZSTD_SUPPORT=1
       LZ4_SUPPORT=1
       LZMA_XZ_SUPPORT=1
       LZO_DIR=#{Formula["lzo"].opt_prefix}
